091dfafee38bb8c782d095e05fce1fda4dd66d4b
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-04-13 22:42:19 +0700

    Tambah route untuk halaman utama

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import RegisterPage from './pages/authentication/register';
 import NotFoundPage from './pages/notfound/notfound';
 import LoginPage from './pages/authentication/login';
+import HomePage from './pages/home/home';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={ <HomePage /> } />
           <Route path='/register' element={ <RegisterPage /> } />
           <Route path='/login' element={ <LoginPage /> } />
           <Route path='*' element={ <NotFoundPage /> } />
