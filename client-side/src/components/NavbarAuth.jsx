cd8d943f84ac56aa88e1c51902043beddcde2437
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-04-13 14:01:18 +0700

    Edit navbar auth

--- client-side/src/components/NavbarAuth.jsx
+++ client-side/src/components/NavbarAuth.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logoBinar from '../assets/binar_logo.png'
 
 const NavbarAuthComponent = () => {
   return (
@@ -7,8 +6,22 @@ const NavbarAuthComponent = () => {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <a class="navbar-brand" href="/">
-            <img src={ logoBinar } alt="img-banner-auth" width="30" height="24" class="d-inline-block align-text-top" />
+            <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="img-banner-auth" width="30" height="24" class="d-inline-block align-text-top" />
+            Binar FSW 2
           </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="/">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">Link</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
