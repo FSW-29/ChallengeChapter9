478c7adaeca280c54b524dbb3c18e1a756f69b75
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-04-14 13:24:06 +0700

    Beri aksi untuk logout

--- client-side/src/pages/home/home.jsx
+++ client-side/src/pages/home/home.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router";
+import { getAuth, signOut } from "firebase/auth";
+import firebase from "../../services/firebase";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    const auth = getAuth(firebase);
+    await signOut(auth);
+
+    localStorage.removeItem('token');
+    navigate('/login');
+  };
+
   return (
     <>
       <h1>Ini adalah homepage</h1>
+      <button className="btn btn-primary" onClick={ handleLogout }>Logout</button>
     </>
   );
 };
