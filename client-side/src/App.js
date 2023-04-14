c33f7f4cf50286d9e64f578889994e18d154e242
deo <wicaksonodeo@gmail.com>
2023-04-14 19:03:04 +0700

    add profile preview

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import RegisterPage from './pages/authentication/register';
 import NotFoundPage from './pages/notfound/notfound';
+import ProfilePage from './pages/authentication/profilePage';
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/register' element={ <RegisterPage /> } />
+          <Route exact path="/profile" element={<ProfilePage/>}/>
           <Route path='*' element={ <NotFoundPage /> } />
+          
         </Routes>
       </BrowserRouter>
     </>
